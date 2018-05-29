0ac9363314efbabdc47f7c57e410e2935a8a1fc1
Koichi <s.koichi@gmail.com>
2018-05-29 17:41:25 -0400

    Setting Up Server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-var express = 'hello';
+var express = require('express');
+var bodyParser = require('body-parser')
+var axios = require('axios')
+
+var app = express(); 
+
+// app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/wordgame', (req, res) => {
+  //need model for querying data from the database 
+  //
+  res.send('Obtained!')
+}
+
+app.post('/wordgame', (req,res) => { 
+  //need model for adding username and score to the database 
+  res.send('Updated')
+
+})
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
