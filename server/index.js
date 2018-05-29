fc594bbe810f48a65899118cb7a8c0dbc91c8136
Scott McCreary <scott.mccreary.11@gmail.com>
2018-05-29 19:02:32 -0400

    Fix bug

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ app.get('/wordgame', (req, res) => {
   //need model for querying data from the database 
   //
   res.send('Obtained!')
-}
+});
 
 app.post('/wordgame', (req,res) => { 
   //need model for adding username and score to the database 
   res.send('Updated')
 
-})
+});
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
