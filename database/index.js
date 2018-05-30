1b378db3132873413586013e02de4a66692bba76
Lina Lei <leilsaru@gmail.com>
2018-05-30 12:25:56 -0400

    got database working

--- database/index.js
+++ database/index.js
@@ -1,32 +1,53 @@
 const mysql = require('mysql');
-const mysqlConfig = require('./config.js');
+// const mysqlConfig = require('./config.js');
 
-const connection = mysql.createConnection(mysqlConfig);
-connection.connect();
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', 
+  password: 'password', 
+  database: 'humptydumpty'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 //FUNCTIONS TO INTERACT WITH DATABASE GO HERE:
-
-//function to add new user
-const addUser = function(user, callback) {
-  let queryStr = `INSERT INTO users VALUES (${user.id}, '${user.username}', ${0})`;
-  connection.query(queryStr, (err, data) => {
-    if(err) console.log('error adding user into DB', err);
-    callback(data);
-  });
-};
 
 //function to retrieve all users
 const retrieveUsers = function(callback) {
   let queryStr = `SELECT * FROM users`;
   connection.query(queryStr, (err, data) => {
-    if(err) console.log('error retrieving users from DB', err);
+    if(err) console.log('DB: error retrieving users', err);
     callback(data);
   });
 };
 
+//function to retrieve high score for a certain user
+const retrieveHighScore = function(user, callback) {
+  let queryStr = `SELECT high_score FROM users WHERE username = '${user.username}'`;
+  connection.query(queryStr, (err, data) => {
+    if(err) console.log('DB: error retrieving high score for a certain user', err);
+    callback(data);
+  });
+};
+
+//function to update high score for a certain user
+const addHighScore = function(userWithScore, callback) {
+  let queryStr = `REPLACE INTO users VALUES (${userWithScore.id}, '${userWithScore.username}', ${userWithScore.high_score}) `;
+  connection.query(queryStr, (err, data) => {
+    if(err) console.log('error adding high score into DB', err);
+    callback(data);
+  });
+};
+
+// addHighScore({id: 10, username: 'egg', high_score: 99}, (data) => {console.log(data)}), 
+// addHighScore({id: 11, username: 'gudetama', high_score: 99999}, (data) => {console.log(data)})
 
 //export all database functions here 
 module.exports = {
-  addUser,
-  retrieveUsers
+  retrieveUsers,
+  retrieveHighScore,
+  addHighScore
 };
